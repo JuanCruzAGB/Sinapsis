4f46ef2627eea6dfc4b1980ea3da7d1a3e2c5f4c
kolujAs <fernandodeibe@gmail.com>
2022-07-10 19:15:04 -0300

    S1W1F Install tailwind & structure of the users table

--- public/js/app.js
+++ public/js/app.js
@@ -49490,8 +49490,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\wamp64\www\Laravel\Sinapsis\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\wamp64\www\Laravel\Sinapsis\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\Sinapsis\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\Sinapsis\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/user/list.blade.php
+++ resources/views/user/list.blade.php
@@ -12,18 +12,43 @@
     <li id="users" class="tab-content opened">
         <section class="grid gap-4">
             <header class="content-title">
-                <h2>Users</h2>
+                <h2 class="text-uppercase text-left mt-4 mb-3 px-2 px-md-0">Candidates</h2>
             </header>
 
             <main class="content-table">
                 <section>
-                    <table class="table grid gap-4">
-                        <tbody class="flex flex-wrap gap-4">
-                            <tr class="flex gap-4">
-                                <td>Something</td>
+                    {{-- <div class="pointer-events mx-0 mb-3 px-2 px-md-0 d-flex align-items-center">
+                        <p class="filter-title mb-0 mr-3">Order by:</p>
+                        <button type="button" data-by="created_at" data-type="ASC" class="filter-candidates filter-order btn btn-two mr-3">
+                            <span class="btn-text">Date</span>
+                            <i class="filter-icon fas fa-chevron-down"></i>
+                        </button>
+                        <button type="button" data-by="full_name" data-type="DESC" class="filter-candidates filter-order btn btn-two">
+                            <span class="btn-text">Name</span>
+                            <i class="filter-icon fas fa-chevron-down"></i>
+                        </button>
+                    </div> --}}
+                </section>
+                <section class="table col-lg-6 mx-2 mx-md-4 mx-lg-0 mb-4 pr-lg-0">
+                    <table class="table">
+                        <tbody>
+                            <tr data-id_candidate="1">
+                                <td class="table-icon"><i class="far fa-user"></i></td>
+                                <td class="candidate_number">1</td><td class="full_name">Exam demo</td>
                             </tr>
                         </tbody>
                     </table>
+                    <div class="filter-pagination">
+                        <div class="paginationjs">
+                            <div class="paginationjs-pages">
+                                <ul>
+                                    <li class="paginationjs-page J-paginationjs-page active" data-num="1">
+                                        <a>1</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </main>
         </section>
